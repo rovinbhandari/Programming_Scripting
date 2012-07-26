11abfba552a4b88270b11e5c0ba0bd233ace35fd
Rovin Bhandari <rovinbhandari@gmail.com>
2012-07-27 03:59:38 +0530

    implemented a new algorithm to solve WCOUNT. commit before debugging.

--- Codechef/FEB12/WCOUNT/testcases.py
+++ Codechef/FEB12/WCOUNT/testcases.py
@@ -4,13 +4,18 @@ import sys
 import string
 import random
 
-t = 5000
+t = 500
 print t
+charset = string.letters
 while t > 0:
 	t -= 1
 	l = random.randint(1, 500)		# random.randint(a, b) is inclusive of a and b
+	charfreq = dict(zip(charset, [10] * 52))
 	s = ''
 	while l > 0:
-		l -= 1
-		s += random.choice(string.letters)
+		c = random.choice(string.letters)
+		if charfreq[c] > 0:
+			charfreq[c] -= 1
+			s += c
+			l -= 1
 	print s
