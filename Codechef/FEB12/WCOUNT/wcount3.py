213585d0c18cd16fa2602c1b96f85a60c14bd580
Rovin Bhandari <rovinbhandari@gmail.com>
2012-07-28 17:43:37 +0530

    debugging the new algorithm to solve WCOUNT.

--- Codechef/FEB12/WCOUNT/wcount3.py
+++ Codechef/FEB12/WCOUNT/wcount3.py
@@ -9,9 +9,9 @@ primes1_500 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 6
 primefactors2_10 = {2: {2: 1, 3: 0, 5: 0, 7: 0}, 3: {2: 1, 3: 1, 5: 0, 7: 0}, 4: {2: 3, 3: 1, 5: 0, 7: 0}, 5: {2: 3, 3: 1, 5: 1, 7: 0}, 6: {2: 4, 3: 2, 5: 1, 7: 0}, 7: {2: 4, 3: 2, 5: 1, 7: 1}, 8: {2: 7, 3: 2, 5: 1, 7: 1}, 9: {2: 7, 3: 4, 5: 1, 7: 1}, 10: {2: 8, 3: 4, 5: 2, 7: 1}}
 
 nr = 0
-dr = [(int)]
+dr = {}
 pfnr = {}
-dbg = not True
+dbg = True
 
 def dbgout(d, x):
 	if dbg:
@@ -20,10 +20,10 @@ def dbgout(d, x):
 
 def setdr(s):
 	global dr
-	dr = []
+	dr = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0}
 	while len(s) > 0:
 		c = s[0]
-		dr.append(s.count(c))
+		dr[s.count(c)] += 1
 		s = s.replace(c, '')
 
 def primepowerfactorial(x, p):
@@ -33,31 +33,33 @@ def primepowerfactorial(x, p):
 		power += x
 	return power
 
-def primefactorize(x):		# x is the number whose factorial is needed to be prime factorized
-	assert (x >= 2)
-	pfx = {}
+def primefactorizenr():
+	global pfnr
+	pfnr = {}
+	intsqrtnr = int(math.sqrt(nr))
 	for p in primes1_500:
-		if p <= x:
-			pfx[p] = primepowerfactorial(x, p)
+		if p <= nr:
+			if p > intsqrtnr:
+				pfnr[p] = nr // p
+			else:
+				pfnr[p] = primepowerfactorial(nr, p)
 		else:
 			break
-	return pfx
 
 def divide():
 	global pfnr
-	pfnr = primefactorize(nr)
+	primefactorizenr()
 	dbgout("pfnr", pfnr)
+	del(dr[1])
 	for d in dr:
-		if d == 1:
-			continue
 		if nr >= 2:
-			pfnr[2] -= primefactors2_10[d][2]
+			pfnr[2] -= dr[d] * primefactors2_10[d][2]
 			if nr >= 3:
-				pfnr[3] -= primefactors2_10[d][3]
+				pfnr[3] -= dr[d] * primefactors2_10[d][3]
 				if nr >= 5:
-					pfnr[5] -= primefactors2_10[d][5]
+					pfnr[5] -= dr[d] * primefactors2_10[d][5]
 					if nr >= 7:
-						pfnr[7] -= primefactors2_10[d][7]
+						pfnr[7] -= dr[d] * primefactors2_10[d][7]
 
 def powermod(a, b):
 	dbgout("a", a)
@@ -69,14 +71,15 @@ def powermod(a, b):
 		if a > mod:
 			a %= mod
 		b -= 1
-	dbgout("a", a)
-	dbgout("b", b)
+	dbgout("a^b", a)
 	return a
 
 def multiply():
 	result = 1
 	for p in pfnr:
+		dbgout("result", result)
 		result = (result * powermod(p, pfnr[p])) % mod
+	dbgout("result", result)
 	return result
 
 t = raw_input()
@@ -93,6 +96,13 @@ for s in sys.stdin:
 		setdr(s)
 		dbgout("dr", dr)
 		divide()
+		dbgout("dr", dr)
 		dbgout("pfnr", pfnr)
 		print multiply()
+"""
 
+nr = 500
+primefactorizenr()
+print pfnr
+
+"""
