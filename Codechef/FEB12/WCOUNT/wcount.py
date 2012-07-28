91eb28550dfe6d900c60266721219f33d205c756
Rovin Bhandari <rovinbhandari@gmail.com>
2012-07-28 17:43:37 +0530

    debugging the new algorithm to solve WCOUNT.

--- Codechef/FEB12/WCOUNT/wcount.py
+++ Codechef/FEB12/WCOUNT/wcount.py
@@ -4,19 +4,35 @@ import math
 import sys
 import string
 
+dbg = True
+
+def dbgout(d, x):
+	if dbg:
+		s = "\t\t" + repr(d) + " : " + repr(x)
+		print s
+
 t = input()
 for s in sys.stdin:
 	s = s.strip()
+	dbgout("s", s)
 	nr = len(s)
+	dbgout("nr", nr)
 	if nr == 0:
 		continue
-	dr = []
-	while len(s) > 0:
-		c = s[0]
-		dr.append(s.count(c))
-		s = s.replace(c, '')
-	result = math.factorial(nr)
-	for i in dr:
-		result /= math.factorial(i)
-	print result % 1000000007
+	elif nr == 1:
+		print 1
+	else:
+		dr = []
+		while len(s) > 0:
+			c = s[0]
+			dr.append(s.count(c))
+			s = s.replace(c, '')
+		dbgout("dr", dr)
+		result = math.factorial(nr)
+		dbgout("result", result)
+		for i in dr:
+			result /= math.factorial(i)
+			dbgout("result", result)
+		result %= 1000000007
+		print result
 sys.exit(0)
