c11bb9ac75177a42c6ce7d6520c236532360e6dd
Rovin Bhandari <rovinbhandari@gmail.com>
2012-11-02 05:50:39 +0530

    Solving Exchange from cs3.interviewstreet.com. Commit before debugging.

--- InterviewStreet/CodeSprint 3/Exchange/exchange_dbg.cpp
+++ InterviewStreet/CodeSprint 3/Exchange/exchange_dbg.cpp
@@ -3,7 +3,10 @@
 #include <bitset>
 #include <cstdio>
 #include <cassert>
+#include <iostream>
 using namespace std;
+
+#define DBG true
 
 typedef unsigned short int	usi;
 #define FORMATusi			"%hu"
@@ -22,12 +25,36 @@ ui sizeexchange;
 
 #define MAP(x, y)	(((k - x) * (k - x - 1)) / 2 + (k - y))
 
+//DBG_BEGIN
+void printbitset()
+{
+	cerr //<< endl
+		 << "\t\t"
+		 << exchange.to_ulong()
+		 << endl;
+	string exchangestr = exchange.to_string();
+	exchangestr = string(exchangestr.end() - sizeexchange, exchangestr.end());
+	for(int sum = 0, i = k - 1; i > 0; i--)
+	{
+		cerr << "\t\t";
+		for(int j = 0; j < k - i - 1; j++)
+			cerr << " ";
+		cerr << string(exchangestr.begin() + sum, exchangestr.begin() + sum + i)
+			 << endl;
+		sum += i;
+	}
+}
+//DBG_END
+
 set<usi> indicescomputed;
 
 void individualcomputetransitions(usi root)
 {
 	assert(indicescomputed.insert(root).second);
-	// isolate the part of bitset used by root.
+	// isolate the part of bitset used by root. //TODO: check if \
+												  vertical part \
+												  needs to be \
+												  checked too.
 	// => from root + 1 to k
 	// check if the size of this part is non-zero. terminate if zero.
 	if(root + 1 == k)
@@ -75,13 +102,25 @@ void smallestpermutation()
 	{
 		usi minvalue = permutation[i], minindex = i;
 		for(usi j = i + 1; j < sizepermutation; j++)
+		{
 			if(exchange[MAP(i, j)] && permutation[j] < minvalue)
 			{
 				minvalue = permutation[j];
 				minindex = j;
 			}
+		}
 		if(minindex != i)
 		{
+			
+			//DBG_BEGIN
+			if(DBG)
+				cerr << "swapping values at indices "
+					 << i
+					 << " and "
+					 << minindex
+					 << endl;
+			//DBG_END
+
 			usi temp = permutation[i];
 			permutation[i] = permutation[minindex];
 			permutation[minindex] = temp;
@@ -93,27 +132,74 @@ int main(void)
 {
 	int i, j;
 	char c;
-	
 	scanf(FORMATusi, &k);
 	sizeexchange = (k * (k - 1)) / 2;
 	
+	//DBG_BEGIN
+	if(DBG)
+		cerr << "sizepermutation = "
+			 << k
+			 << endl
+			 << "sizeexchange = "
+			 << sizeexchange
+			 << endl;
+	//DBG_END
+	
 	for(i = 1; i < sizepermutation; i++)
 		scanf(FORMATusi, &permutation[i]);
+	
+	//DBG_BEGIN
+	if(DBG)
+	{
+		cerr << "permutation :";
+		for(i = 0; i < sizepermutation; i++)
+			cerr << " "
+				 << permutation[i];
+		cerr << endl;
+	}
+	//DBG_END
 	
 	i = j = 1;
 	while(c = getchar())
 	{
 		if(!(c == 'N' || c == 'Y'))
 			continue;
+		
+		//DBG_BEGIN
+		if(DBG)
+			cerr << "read '"
+				 << c
+				 << "' at ("
+				 << i
+				 << ", "
+				 << j
+				 << ")"
+				 << endl;
+		//DBG_END
 
 		if(i < j)
 		{
+			
+			//DBG_BEGIN
+			if(DBG)
+				cerr << "storing..."
+					 << endl;
+			//DBG_END
+			
 			if(c == 'N')
 				exchange.set(MAP(i, j), false);
 			else if(c == 'Y')
 				exchange.set(MAP(i, j), true);
 		}
 		
+		//DBG_BEGIN
+		if(0 && DBG)
+		{
+			cerr << "exchange :\t";
+			printbitset();
+		}
+		//DBG_END
+
 		j++;
 		if(j == sizepermutation)
 		{
@@ -124,9 +210,33 @@ int main(void)
 			break;
 	}
 
+	//DBG_BEGIN
+	if(DBG)
+	{
+		cerr << "exchange :\t";
+		printbitset();
+	}
+	//DBG_END
+
 	computetransitions();
 
+	//DBG_BEGIN
+	if(DBG)
+	{
+		cerr << "exchange :\t";
+		printbitset();
+	}
+	//DBG_END
+
 	smallestpermutation();
+
+	//DBG_BEGIN
+	cerr << "smallest permutation :";
+	for(i = 0; i < sizepermutation; i++)
+		cerr << " "
+			 << permutation[i];
+	cerr << endl;
+	//DBG_END
 
 	for(i = 1; i < sizepermutation; i++)
 		printf(FORMATusi " ", permutation[i]);
