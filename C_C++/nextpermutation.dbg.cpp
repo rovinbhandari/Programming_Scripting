ad745978380b269eac7b81657f1fc21a8fa6f76d
Rovin Bhandari <rovinbhandari@gmail.com>
2012-11-08 23:35:55 +0530

    next permutation implemented and tested.

--- C_C++/nextpermutation.dbg.cpp
+++ C_C++/nextpermutation.dbg.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 using namespace std;
 
+#define DBG false
+
 #define INF	numeric_limits<int>::max()
 
 void numstrtovector(string ns, vector<int>& v)
@@ -24,6 +26,14 @@ int vectortonum(vector<int>& v)
 	return n;
 }
 
+void printvector(vector<int>& v)
+{
+	for(vector<int>::iterator it = v.begin();
+			it != v.end(); it++)
+		cerr << *it << " ";
+	cerr << endl;
+}
+
 void reversevector(vector<int>::iterator beg, vector<int>::iterator end)
 {
 	if(beg == end)
@@ -41,12 +51,8 @@ bool nextpermutation(vector<int>& v)
 {
 	if(v.size() < 2)
 		return true;
-	// find the point at which the sequence (starting from the end) \
-	becomes decreasing. find the smallest element in that sequence \
-	which is just greater than the first element at which the sequence \
-	becomes decreasing. swap them and reverse the sequence.
-	// if the sequence never becomes decreasing, simply return \
-	the reversed sequence.
+	// find the point at which the sequence (starting from the end) becomes decreasing. find the smallest element in that sequence which is just greater than the first element at which the sequence becomes decreasing. swap them and reverse the sequence.
+	// if the sequence never becomes decreasing, simply return the reversed sequence.
 	int min = INF, minindex;
 	int i = v.size() - 1, prev = v[i], cur;
 	bool founddecrseq = false;
@@ -67,25 +73,46 @@ bool nextpermutation(vector<int>& v)
 		int elementtobeswappedindex = i - 1;
 		while(i < v.size())
 		{
-			//XXX: notice the following conditions!!!
-			if(v[i] <= min && v[i] > v[elementtobeswappedindex])
+			if(v[i] <= min && v[i] > v[elementtobeswappedindex])	//XXX: notice the conditions!!!
 			{
 				min = v[i];
 				minindex = i;
 			}
 			i++;
 		}
+		
+		if(DBG)
+			cerr << "seqstartindex : "
+				 << seqstartindex
+				 << "\nelement to be swapped index : "
+				 << elementtobeswappedindex
+				 << "\nminindex : "
+				 << minindex
+				 << endl;
+
 		int temp = v[elementtobeswappedindex];
 		v[elementtobeswappedindex] = v[minindex];
 		v[minindex] = temp;
+
+		if(DBG)
+			cerr << "vector post swap : ";
+		if(DBG)
+			printvector(v);
+
+		if(DBG)
+			cerr << "reversing between indices : "
+				 << seqstartindex
+				 << " and "
+				 << v.size() - 1
+				 << endl;
+
 		reversevector(v.begin() + seqstartindex, v.end());
 		return true;
 	}
 	else
 	{
 		reversevector(v.begin(), v.end());
-		return false;	// when the next higher permutation does \
-		not exist and the first permutation is returned.
+		return false;	// when the next higher permutation does not exist and the first permutation is returned.
 	}
 }
 
@@ -103,15 +130,24 @@ int main(int argc, char* argv[])
 			return 0;
 		numstrtovector(ns, num);
 
+		if(DBG)
+			printvector(num);
+
 		if(useinbuilt)
 			next_permutation(num.begin(), num.end());
 		else
 			nextpermutation(num);
 		
+		if(DBG)
+			printvector(num);
+
 		cout << ns
 			 << " --> "
 			 << vectortonum(num)
 			 << endl;
+		
+		if(DBG)
+			cerr << endl;
 	}
 	return 0;
 }
