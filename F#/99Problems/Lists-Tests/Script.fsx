0b6218bd79a9cfcccc0fc69aac2fcbfbfae5d741
Rovin Bhandari <rovinbhandari@gmail.com>
2017-03-06 04:01:14 +0100

    Project with unit-test working. Had to use bindingRedirect because of a bug with FSharp.Core reference.

--- F#/99Problems/Lists-Tests/Script.fsx
+++
@@ -1,6 +0,0 @@
-﻿
-#load "Lists_Tests.fs"
-open Lists_Tests
-
-// Define your library scripting code here
-
---
+++ FSharp/99Problems/ListsTests/ListsTests.fs
@@ -0,0 +1,11 @@
+﻿module ListsTestz
+
+open NUnit.Framework
+open FsCheck
+open FsUnit
+open Lists
+
+[<Test>]
+let ``check last element of valid list``() = 
+    let res = Lists._1to10.Last [5..10]
+    res |> should equal 10
