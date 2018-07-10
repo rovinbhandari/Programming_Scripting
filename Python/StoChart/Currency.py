f2950fd153b05c4a66a925fd059b1024f9d6d424
Rovin Bhandari <rovinbhandari@gmail.com>
2018-07-10 13:09:41 +0200

    Currency handling testing pending.

--- Python/StoChart/Currency.py
+++ Python/StoChart/Currency.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime, date
 import csv
 import requests
+from math import isclose
 
 class Currency:
     bizday_title = "BizDay"
@@ -13,7 +14,12 @@ class Currency:
         self.conversion = float(conversion_str)
 
     def __lt__(self, other):
-        return self.bizday < other.bizday
+        if self.bizday < other.bizday:
+            return True
+        elif self.bizday == other.bizday:
+            return isclose(self.conversion, other.conversion, abs_tol=1e-5)
+        else:
+            return False
         
     def __str__(self):
         return "{0},{1:.6f}".format(self.bizday.strftime("%Y-%m-%d"), self.conversion)
@@ -22,9 +28,12 @@ class Currency:
 # This class can be converted to a base class, with specific currency classes implementing their own
 # getters and modifiers.
 class NOK:
-    def __init__(self, start_date, file_dir = ".", file_name = "USDNOK.daily.csv"):
+    def __init__(self, start_date, end_date = None, file_dir = ".", file_name = "USDNOK.daily.csv"):
         self.start_date = start_date
         self.file_path = os.path.join(file_dir, file_name)
+        self.end_date = date.today() if end_date is None else end_date
+        if self.end_date < self.start_date:
+            raise Exception("End date cannot be less than start date!")
     
     def DictReaderToList(self, reader):
         l = [Currency(r[Currency.bizday_title], r[Currency.conversion_title]) for r in reader]
@@ -45,7 +54,36 @@ class NOK:
             content = download.content.decode('utf-8')
             return self.DictReaderToList(csv.DictReader(content.splitlines()))
     
+    def UpdateCache(self, values):
+        with open(self.file_path, 'w') as f:
+            f.write("{0},{1}".format(self.bizday_title, self.conversion_title))
+            for v in values:
+                f.write(str(v))
+
     def GetData(self):
-        from_cache = self.GetDataFromCache()
-        cache_start_date = from_cache[0].bizday
-        #TODO
+        cache = self.GetDataFromCache()
+        nb_start_date = self.start_date
+        if len(cache) > 0:
+            cache_start_date = cache[0].bizday
+            cache_end_date = cache[-1].bizday
+            #    ==========csd===========ced===========
+            # 1:  ^nsd
+            # 2:                 ^nsd
+            # 3:                                ^nsd
+            # Case 1: fetch all
+            # Case 2, 3: fetch from ced
+            if nb_start_date > cache_start_date:
+                nb_start_date = cache_end_date
+        from_nb = self.GetDataFromNorgesBank(nb_start_date)
+        new_values = [e for e in from_nb if not (e in cache)]
+        cache.extend(new_values)
+        cache.sort()
+        self.UpdateCache(cache)
+        return [v for v in cache if v.bizday >= self.start_date and v.bizday <= self.end_date]
+
+def Test(sd, ed):
+    n = NOK(start_date = sd, end_date = ed)
+    c = GetData()
+    print sum([v.conversion for v in c])/float(len(c))
+
+Test(date(2017, 6, 25), date(2018, 6, 25))
