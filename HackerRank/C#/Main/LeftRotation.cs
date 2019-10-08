b49d14b2c1a9638b2f55c4840e10746770644f73
Rovin Bhandari <rovinbhandari@gmail.com>
2019-10-08 21:26:29 +0200

    HackerRank problem: Arrays - Left Rotation

---
+++ HackerRank/C#/Main/LeftRotation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    using System.Linq;
+
+    public class LeftRotation
+    {
+        public static void Runner()
+        {
+            var nd = Console.ReadLine().Split(' ').Select(int.Parse);
+            var arr = Console.ReadLine().Split(' ').Select(s => int.Parse((string)s)).ToArray();
+            var newArr = Logic(arr, nd.Last());
+            Console.WriteLine(string.Join<int>(" ", newArr));
+        }
+
+        public static IEnumerable<int> Logic(int[] arr, int d)
+        {
+            var l = arr.Length;
+            var rotations = d % l;
+            foreach (var i in arr.Skip(rotations))
+            {
+                yield return i;
+            }
+
+            foreach (var i in arr.Take(rotations))
+            {
+                yield return i;
+            }
+        }
+    }
+}
