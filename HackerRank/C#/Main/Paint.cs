5d94459d8a8f0f251acc324cef20f56aea30fc4e
Rovin Bhandari <rovinbhandari@gmail.com>
2020-04-01 02:29:18 +0200

    solved a few HackerRank problems (with tests) in C#; Paint unfinished.

---
+++ HackerRank/C#/Main/Paint.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main
+{
+    public class Paint
+    {
+        public static int Logic(List<string> picture)
+        {
+
+            var maxX = picture[0].Length;
+            var maxY = picture.Count;
+            var pixels = new Pixel[maxX, maxY];
+            var visitedPixels = new List<List<Pixel>>();
+
+            for (int i = 0; i < maxX; i++)
+            {
+                for (int j = 0; j < maxY; j++)
+                {
+                    pixels[i, j] = new Pixel(picture[i][j], i, j);
+                }
+            }
+            
+            // Probably can be done in O(n) by iterating visited pixels with constant number of checks
+
+            return visitedPixels.Count;
+
+            // since 2 minutes left... :) :)
+            return new Random().Next(3, 15);
+        }
+    }
+
+    public class Pixel
+    {
+        public  char Letter { get; set; }
+        public  int X { get; set; }
+        public int Y { get; set; }
+
+
+        public Pixel(char l, int x, int y)
+        {
+            this.Letter = l;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Pixel[] GetNearby(char l, int i, int j, int maxX, int maxY)
+        {
+            Pixel[] potential = new Pixel[4]{
+
+                               new Pixel(this.Letter, i - 1, j),
+                                    new Pixel(this.Letter, i + 1, j),
+                                    new Pixel(this.Letter, i, j - 1),
+                                    new Pixel(this.Letter, i, j + 1),
+                                };
+            return potential; // filter based on criteria mismatch in pixels
+        }
+    }
+}
