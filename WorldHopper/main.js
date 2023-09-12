33b877de2d42a29d0256a177533a18cd388a3f5c
Rovin Bhandari <rovinbhandari@gmail.com>
2023-09-12 17:34:35 +0200

    try a more complex example of multicoloured box.

--- WorldHopper/main.js
+++ WorldHopper/main.js
@@ -1,27 +1,61 @@
 import * as THREE from 'three';
+let camera, scene, renderer, mesh;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+init();
+animate();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+function init() {
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
 
-camera.position.z = 5;
+    scene = new THREE.Scene();
 
+    const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 ).toNonIndexed();
+		
+		// vertexColors must be true so vertex colors can be used in the shader
+		
+    const material = new THREE.MeshBasicMaterial( { vertexColors: true } ); 
+		
+		// generate color data for each vertex
+		
+		const positionAttribute = geometry.getAttribute( 'position' );
+		
+		const colors = [];
+		const color = new THREE.Color();
+		
+		for ( let i = 0; i < positionAttribute.count; i += 3 ) {
+		
+			color.set( Math.random() * 0xffffff );
+			
+			// define the same color for each vertex of a triangle
+			
+			colors.push( color.r, color.g, color.b );
+			colors.push( color.r, color.g, color.b );
+			colors.push( color.r, color.g, color.b );
+		
+		}
+		
+		// define the new attribute
+		
+		geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
 
-function animate() {
-	requestAnimationFrame( animate );
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-	renderer.render( scene, camera );
 }
 
-animate();
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render( scene, camera );
+
+}
