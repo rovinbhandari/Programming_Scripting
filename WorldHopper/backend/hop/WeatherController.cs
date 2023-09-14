f1d62d8f866bff01d79d4604fbe0f7f8e6200329
Rovin Bhandari <rovinbhandari@gmail.com>
2023-09-14 16:27:44 +0200

    Async call to backend to update camera angles [FINALLY!!] works.

---
+++ WorldHopper/backend/hop/WeatherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hop;
+
+[ApiController]
+public class WeatherController : Controller
+{
+    [HttpGet]
+    [Route("/weatherforecast")]
+    public IActionResult GetWeatherForecast()
+    {
+        var summaries = new[]
+{
+"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                       new WeatherForecast
+                                      (
+                                              DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                                                                 Random.Shared.Next(-20, 55),
+                                                                                    summaries[Random.Shared.Next(summaries.Length)]
+                                                                                                   ))
+            .ToArray();
+        return Ok(forecast);
+    }
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
--- WorldHopper/frontend/earth.js
+++ WorldHopper/frontend/earth.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 
+const backendPort = 21248; // TODO: obtain from env variable or config
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -15,15 +16,40 @@ scene.add(earth);
 
 camera.position.z = 15;
 
+var cameraAngleDeltas = {x: 0.005, y: 0.005, z: 0.005};
+const refreshButton = document.getElementById('refreshButton');
+refreshButton.addEventListener('click', async function() { await RefreshAngles() });
+
 animate();
 
 function animate() {
     requestAnimationFrame(animate);
 
     // Add any animations or updates here
-	earth.rotation.x += 0.005;
-	earth.rotation.y += 0.005;
-    earth.rotation.z += 0.005;
+	earth.rotation.x += cameraAngleDeltas.x;
+	earth.rotation.y += cameraAngleDeltas.y;
+    earth.rotation.z += cameraAngleDeltas.z;
 
     renderer.render(scene, camera);
+}
+
+async function RefreshAngles() {
+    // TODO:
+    // To avoid CORS issues, instead of modifying the backend,
+    // this could be tried as well:
+    // https://javascript.info/fetch-crossorigin#using-scripts
+    const response = await fetch(
+        `http://localhost:${backendPort}/cameraangles`,
+        {
+            method: 'GET',
+            headers:
+            { 
+                'Content-Type': 'application/json'
+            }
+        });
+    const responseJson = await response.json(); //extract JSON from the http response
+    cameraAngleDeltas.x = responseJson[0].x;
+    cameraAngleDeltas.y = responseJson[0].y;
+    cameraAngleDeltas.z = responseJson[0].z;
+    console.log(cameraAngleDeltas);
 }
