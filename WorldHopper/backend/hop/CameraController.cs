881e59e013630f38fceca503c28fe835c4f33492
Rovin Bhandari <rovinbhandari@gmail.com>
2023-09-14 16:27:44 +0200

    Async call to backend to update camera angles [FINALLY!!] works.

---
+++ WorldHopper/backend/hop/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hop;
+
+[Route("cameraangles")]
+[ApiController]
+[EnableCors("whop frontend")]
+public class CameraController : Controller
+{
+    [HttpGet]
+    public IActionResult GetCameraAngles()
+    {
+        var random = new Random(Seed: DateTime.Now.Millisecond);
+        Func<double> randomVal = () => random.NextDouble() / 100.0d;
+        var angles = new[]
+        {
+            new { X = randomVal(), Y = randomVal(), Z = randomVal() },
+        };
+        HttpContext.Response.Headers.Append("Access-Control-Allow-Origin", "http://localhost:5173");
+        HttpContext.Response.Headers.Append("Access-Control-Allow-Methods", "GET");
+        return Ok(angles);
+    }
+
+    [HttpOptions]
+    [HttpHead]
+    public IActionResult GetCameraAnglesOptions()
+    {
+        HttpContext.Response.Headers.Append("Access-Control-Allow-Origin", "http://localhost:5173");
+        HttpContext.Response.Headers.Append("Access-Control-Allow-Methods", "GET");
+        return Ok();
+    }
+}
