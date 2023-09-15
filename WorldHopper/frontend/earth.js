3517a78583f7e48c1bb9002cfc30795a63083a4d
Rovin Bhandari <rovinbhandari@gmail.com>
2023-09-15 12:19:44 +0200

    adding a crude "point" works now.

--- WorldHopper/frontend/earth.js
+++ WorldHopper/frontend/earth.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 
 const backendPort = 21248; // TODO: obtain from env variable or config
+
+const earthRadius = 6;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -8,9 +11,43 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(5, 32, 32);
+const geometry = new THREE.SphereGeometry(earthRadius, 32, 32);
 const material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('/static/earth/earth-satellite.jpg') });
 const earth = new THREE.Mesh(geometry, material);
+
+const lat = 60;
+const lon = 10;
+const pointLevitation = 0;
+/*
+const pointGeometry = new THREE.SphereGeometry(earthRadius/100, 32, 32);
+const pointMaterial = new THREE.PointsMaterial({ color: 0xff0000 });
+const point = new THREE.Points(pointGeometry, pointMaterial);
+scene.add(point);
+const phi = Math.PI / 4; // Example latitude in radians
+const theta = Math.PI / 2; // Example longitude in radians
+const x = (1.0 + pointLevitation) * earthRadius * Math.sin(phi) * Math.cos(theta);
+const y = (1.0 + pointLevitation) * earthRadius * Math.sin(phi) * Math.sin(theta);
+const z = (1.0 + pointLevitation) * earthRadius * Math.cos(phi);
+pointGeometry.ve .push(new THREE.Vector3(x, y, z));
+*/
+
+const sphericalCoords = {
+    radius: earthRadius, // Radius of the sphere
+    polar: Math.PI / 4,  // Latitude in radians (example: 45 degrees)
+    azimuthal: Math.PI   // Longitude in radians (example: 180 degrees)
+};
+
+const cartesianCoords = sphericalToCartesian(
+    sphericalCoords.radius,
+    sphericalCoords.polar,
+    sphericalCoords.azimuthal
+);
+
+const pointGeometry = new THREE.BufferGeometry().setFromPoints([cartesianCoords]);
+const pointMaterial = new THREE.PointsMaterial({ color: 0xff0000 }); // Red color
+const point = new THREE.Points(pointGeometry, pointMaterial);
+
+earth.add(point); // Add the point to the sphere
 
 scene.add(earth);
 
@@ -52,6 +89,13 @@ async function RefreshAngles() {
     cameraAngleDeltas.y = responseJson[0].y;
     cameraAngleDeltas.z = responseJson[0].z;
     console.log(cameraAngleDeltas);
+}
+
+function sphericalToCartesian(radius, polar, azimuthal) {
+    const x = radius * Math.sin(polar) * Math.cos(azimuthal);
+    const y = radius * Math.sin(polar) * Math.sin(azimuthal);
+    const z = radius * Math.cos(polar);
+    return new THREE.Vector3(x, y, z);
 }
 
 // TODO: next thing to figure out is how to get a point on the earth's surface using coordinates.
