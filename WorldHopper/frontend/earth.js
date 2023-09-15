268c97350c87ae6e12dd0b12f21c65d8c2fdff0e
Rovin Bhandari <rovinbhandari@gmail.com>
2023-09-15 13:11:03 +0200

    adding a point based on latitude and longitude is possible now, but the position of the point is incorrect because the texture (map) is not centered at 0 degrees longitude; need to add an offset to make it work. WIP.

--- WorldHopper/frontend/earth.js
+++ WorldHopper/frontend/earth.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-const backendPort = 21248; // TODO: obtain from env variable or config
+const backendPort = 5206; // TODO: obtain from env variable or config
 
 const earthRadius = 6;
 
@@ -16,20 +16,13 @@ const material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().lo
 const earth = new THREE.Mesh(geometry, material);
 
 const lat = 60;
-const lon = 10;
-const pointLevitation = 0;
+const lon = 10.75;
+const pointLevitation = -0.05; // center of the point relative to the earth's surface
 
-
-const sphericalCoords = {
-    radius: earthRadius, // Radius of the sphere
-    polar: Math.PI / 4,  // Latitude in radians (example: 45 degrees)
-    azimuthal: Math.PI   // Longitude in radians (example: 180 degrees)
-};
-
-const cartesianCoords = sphericalToCartesian(
-    sphericalCoords.radius,
-    sphericalCoords.polar,
-    sphericalCoords.azimuthal
+const cartesianCoords = latLongToVector3(
+    lat,
+    lon,
+    earthRadius
 );
 
 const smallSphereGeometry = new THREE.SphereGeometry(earthRadius/50.0, 32, 32);
@@ -37,10 +30,9 @@ const smallSphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const smallSphere = new THREE.Mesh(smallSphereGeometry, smallSphereMaterial);
 
 // Position the small sphere on top of the big sphere
-smallSphere.position.set(0, earthRadius, 0); // Adjust the position as needed
+smallSphere.position.set(cartesianCoords.x, cartesianCoords.y, cartesianCoords.z); // Adjust the position as needed
 
 earth.add(smallSphere);
-
 scene.add(earth);
 
 camera.position.z = 15;
@@ -81,6 +73,20 @@ async function RefreshAngles() {
     cameraAngleDeltas.y = responseJson[0].y;
     cameraAngleDeltas.z = responseJson[0].z;
     console.log(cameraAngleDeltas);
+}
+
+function latLongToVector3(lat, lon, radius) {
+    // Convert latitude and longitude from degrees to radians
+    const phi = ((90 - lat)/180.0) * Math.PI;
+    const theta = ((lon + 180)/180.0) * Math.PI;
+
+    // Calculate the Cartesian coordinates
+    const x = radius * Math.sin(phi) * Math.cos(theta);
+    const y = radius * Math.cos(phi);
+    const z = radius * Math.sin(phi) * Math.sin(theta);
+
+    console.log(`latLongToVector3(${lat}, ${lon}, ${radius}) = x: ${x}, y: ${y}, z: ${z}`);
+    return new THREE.Vector3(x, y, z);
 }
 
 function sphericalToCartesian(radius, polar, azimuthal) {
